c544116c47d38aa6201397bab3426d0c1b433cd3
Anderson Dias <andersondaraujo@gmail.com>
2009-11-19 17:39:16 -0300

    Removing unnecessary methods and fixing instance methods

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -19,30 +19,18 @@ module Juixe
           has_many association_name, :as => :commentable, :dependent => :destroy, :class_name => 'Comment', :conditions => {:comment_type => association_name}
 
           self.class_eval do
-            # Helper method to lookup for comments for a given object.
-            # This method is equivalent to obj.comments.
-            define_method "find_#{association_name}_for" do |obj|
-              commentable = ActiveRecord::Base.send(:class_name_of_active_record_descendant, self).to_s
-              Comment.find_comments_for_commentable_by_type(commentable, obj.id, association_name)
+            # Helper method to sort comments by date
+            define_method "#{association_name}_ordered_by_submitted" do
+              Comment.find(:all,
+                :conditions => ["commentable_id = ? and commentable_type = ? and comment_type = ?", id, self.class.name, association_name],
+                :order => "created_at DESC"
+              )
             end
 
-            # Helper class method to lookup comments for
-            # the mixin commentable type written by a given user.
-            # This method is NOT equivalent to Comment.find_comments_for_user
-            define_method "find_#{association_name}_by_user" do |user|
-              commentable = ActiveRecord::Base.send(:class_name_of_active_record_descendant, self).to_s
-              Comment.find_comments_by_user_and_type(commentable, obj.id, association_name)
+            # Helper method that defaults the submitted time.
+            define_method "add_#{association_name.singularize}" do |comment|
+              self.send("#{association_name}") << comment
             end
-          end
-
-          # Helper method to sort comments by date
-          define_method "#{association_name}_ordered_by_submitted" do
-            Comment.find_comments_for_commentable_by_type(self.class.name, id, association_name).in_order
-          end
-
-          # Helper method that defaults the submitted time.
-          define_method "add_#{association_name.singularize}" do |comment|
-            self.send("#{association_name}") << comment
           end
         end
       end
